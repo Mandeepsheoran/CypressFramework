785b550bde687c8e8668eb38f5fdcbdca194a6a8
Mandeepsheoran <70449523+Mandeepsheoran@users.noreply.github.com>
2023-02-08 16:16:45 +0400

    Update navigation.spec.js

--- cypress/integration/2-advanced-examples/navigation.spec.js
+++ cypress/integration/2-advanced-examples/navigation.spec.js
@@ -8,7 +8,7 @@ context('Navigation', () => {
   })
 
   it('cy.go() - go back or forward in the browser\'s history', () => {
-    // https://on.cypress.io/go
+  
 
     cy.location('pathname').should('include', 'navigation')
 
@@ -18,29 +18,24 @@ context('Navigation', () => {
     cy.go('forward')
     cy.location('pathname').should('include', 'navigation')
 
-    // clicking back
+ 
     cy.go(-1)
     cy.location('pathname').should('not.include', 'navigation')
 
-    // clicking forward
     cy.go(1)
     cy.location('pathname').should('include', 'navigation')
   })
 
   it('cy.reload() - reload the page', () => {
-    // https://on.cypress.io/reload
+  
     cy.reload()
 
-    // reload the page without using the cache
+
     cy.reload(true)
   })
 
   it('cy.visit() - visit a remote url', () => {
-    // https://on.cypress.io/visit
-
-    // Visit any sub-domain of your current domain
-
-    // Pass options to the visit
+   
     cy.visit('https://example.cypress.io/commands/navigation', {
       timeout: 50000, // increase total time for the visit to resolve
       onBeforeLoad (contentWindow) {
