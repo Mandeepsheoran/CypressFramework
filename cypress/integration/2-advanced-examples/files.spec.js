0926bc7e19034034cb1f54a87adb12af9ba47dcf
Mandeepsheoran <70449523+Mandeepsheoran@users.noreply.github.com>
2023-02-08 14:39:14 +0400

    updated file path

--- cypress/integration/2-advanced-examples/files.spec.js
+++ cypress/integration/2-advanced-examples/files.spec.js
@@ -17,17 +17,10 @@ context('Files', () => {
   })
 
   it('cy.fixture() - load a fixture', () => {
-    // https://on.cypress.io/fixture
-
-    // Instead of writing a response inline you can
-    // use a fixture file's content.
-
-    // when application makes an Ajax request matching "GET **/comments/*"
-    // Cypress will intercept it and reply with the object in `example.json` fixture
+  
     cy.intercept('GET', '**/comments/*', { fixture: 'example.json' }).as('getComment')
 
-    // we have code that gets a comment when
-    // the button is clicked in scripts.js
+  
     cy.get('.fixture-btn').click()
 
     cy.wait('@getComment').its('response.body')
@@ -36,34 +29,24 @@ context('Files', () => {
   })
 
   it('cy.fixture() or require - load a fixture', function () {
-    // we are inside the "function () { ... }"
-    // callback and can use test context object "this"
-    // "this.example" was loaded in "beforeEach" function callback
+  
     expect(this.example, 'fixture in the test context')
       .to.deep.equal(requiredExample)
 
-    // or use "cy.wrap" and "should('deep.equal', ...)" assertion
+   
     cy.wrap(this.example)
       .should('deep.equal', requiredExample)
   })
 
   it('cy.readFile() - read file contents', () => {
-    // https://on.cypress.io/readfile
-
-    // You can read a file and yield its contents
-    // The filePath is relative to your project's root.
+   
     cy.readFile('cypress.json').then((json) => {
       expect(json).to.be.an('object')
     })
   })
 
   it('cy.writeFile() - write to a file', () => {
-    // https://on.cypress.io/writefile
-
-    // You can write to a file
-
-    // Use a response from a request to automatically
-    // generate a fixture file for use later
+ 
     cy.request('https://jsonplaceholder.cypress.io/users')
       .then((response) => {
         cy.writeFile('cypress/fixtures/users.json', response.body)
@@ -73,8 +56,7 @@ context('Files', () => {
       expect(users[0].name).to.exist
     })
 
-    // JavaScript arrays and objects are stringified
-    // and formatted into text.
+
     cy.writeFile('cypress/fixtures/profile.json', {
       id: 8739,
       name: 'Jane',
